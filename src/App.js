ba2bdc3c4d4b69c9c175049434d66e46e76bd083
CoolWarm <78597498+CoolWarm@users.noreply.github.com>
2021-07-14 10:09:41 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+// export default class App extends React.Component {
+//   constructor() {
+//     super();
+//   }
+
+//   render() {
+//     return (
+//       <h1>Hello World</h1>
+//     );
+//   }
+// }
+
+// 没有 生命周期函数
+// 没有 this对象
+// 没有内部状态 state
+export default function() {
+  return (<h1>Hello React</h1>);
+}
