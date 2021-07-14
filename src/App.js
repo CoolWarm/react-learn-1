62749b158d61f9e3688c013f669865cd87dcda14
CoolWarm <78597498+CoolWarm@users.noreply.github.com>
2021-07-14 11:36:20 +0800

    Lifecycle of Component
    组件的生命周期

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import React from "react";
 
-// export default class App extends React.Component {
-//   constructor() {
-//     super();
-//   }
-//   render() {
-//     return (
-//       <h1>Hello World</h1>
-//     );
-//   }
-// }
+export default class App extends React.Component {
+  constructor() {
+    super();
+    console.log("constructor();");
+    this.state = {
+      count: 0,
+      hasChild: true
+    };
+  }
 
-// 没有 生命周期函数
-// 没有 this对象
-// 没有内部状态 state
-export default function() {
-  return (<h1>Hello React</h1>);
+  render() {
+    console.log("render();");
+    return (
+      <div>
+        <h2>Count: {this.state.count}</h2>
+        <button onClick={e => this.add()}>+1</button>
+        <hr />
+        <button onClick={e => this.toggleChild()}>Toggle Child</button>
+        {this.state.hasChild && <Cpn />}
+      </div>
+    );
+  }
+
+  add() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  toggleChild() {
+    this.setState({
+      hasChild: !this.state.hasChild
+    });
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount();");
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate();");
+  }
+}
+
+class Cpn extends React.Component {
+  render() {
+    return <h1>A Child Component</h1>;
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount();");
+  }
 }
