aada549847bae6ac26a1f492a312a7e21027e3bc
CoolWarm <78597498+CoolWarm@users.noreply.github.com>
2021-07-15 10:22:35 +0800

    parent to child (function component)
    组件通讯
    函数式组件 父传子

--- src/App.js
+++ src/App.js
@@ -11,21 +11,13 @@ export default class App extends React.Component {
   }
 }
 
-class Child extends React.Component {
-  // 默认
-  // constructor(props) {
-  //   super(props);
-  //   // this.props = props;
-  // }
-
-  render() {
-    return (
-      <div>
-        <hr />
-        <h2>Name: {this.props.name}</h2>
-        <h2>Age: {this.props.age}</h2>
-        <hr />
-      </div>
-    );
-  }
+function Child(props) {
+  const { name, age } = props;
+  return (
+    <div>
+      <h2>Name: {name}</h2>
+      <h2>Age: {age}</h2>
+      <hr />
+    </div>
+  );
 }
