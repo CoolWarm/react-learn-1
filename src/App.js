f707cf0874c8365bc8cd902af2647f2a871e4183
CoolWarm <78597498+CoolWarm@users.noreply.github.com>
2021-07-15 11:37:40 +0800

    PropTypes of component
    属性验证
    组件 父传子

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
+// https://react.docschina.org/docs/typechecking-with-proptypes.html
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Child name="Harry Potter" age="20" />
-        <Child name="Peter Parker" age="18" />
+        <ClassChild name="Harry Potter" age={20} hobbies={["magic", "learning"]} />
+        <FunctionChild name="Peter Parker" age={18} hobbies={["hanging", "fighting"]} />
+        <FunctionChild />
+        <ClassChild />
       </div>
     );
   }
 }
 
-function Child(props) {
-  const { name, age } = props;
+function FunctionChild(props) {
+  const { name, age, hobbies } = props;
   return (
     <div>
       <h2>Name: {name}</h2>
       <h2>Age: {age}</h2>
+      <ul>
+        {hobbies.map((item) => {
+          return <li>{item}</li>;
+        })}
+      </ul>
       <hr />
     </div>
   );
+}
+
+FunctionChild.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  hobbies: PropTypes.array.isRequired
+}
+
+FunctionChild.defaultProps = {
+  name: "Harry Potter",
+  age: 18,
+  hobbies: []
+}
+
+
+class ClassChild extends React.Component {
+  render() {
+    const { name, age, hobbies } = this.props;
+    return (
+      <div>
+        <h2>Name: {name}</h2>
+        <h2>Age: {age}</h2>
+        <ul>
+          {
+            hobbies.map((item) => {
+              return <li>{item}</li>
+            })
+          }
+        </ul>
+        <hr />
+      </div>
+    );
+  };
+  
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number,
+    hobbies: PropTypes.array.isRequired
+  }
+
+  static defaultProps = {
+    name: "Peter Parter",
+    age: 19,
+    hobbies: []
+  }
 }
