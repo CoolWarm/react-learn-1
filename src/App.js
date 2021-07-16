0146958e4f15ab339c38c5e9e7c4260c3d74b9c8
CoolWarm <78597498+CoolWarm@users.noreply.github.com>
2021-07-16 22:04:03 +0800

    cross-component communication(traditional)
    跨组件通信（传统方式）
    一层一层往下传

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import React from "react";
-import TopBarChildren from "./TopBar-children";
-import TopBar from "./TopBar-props";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Harry Potter",
+      age: 21
+    }
+  }
   render() {
+    const { name, age } = this.state;
     return (
       <div>
-        <TopBarChildren>
-          <button>{"<"}</button>
-          <em>Hello World</em>
-          <a href="/">Go</a>
-        </TopBarChildren>
-        <TopBar left={<button>{"<"}</button>}
-          center={<em>Hello World</em>}
-          right={<a href="/">Go</a>} />
+        <h1>L</h1>
+        <Profile name={name} age={age} />
+        <h1>L</h1>
       </div>
     );
   }
+}
+
+function Profile(props) {
+  const { name, age } = props;
+  return (
+    <div>
+      <h2>M</h2>
+      <Information name={name} age={age} />
+      <h2>M</h2>
+    </div>
+  );
+}
+
+function Information(props) {
+  const {name, age} = props;
+  return (
+    <div>
+      <h3>S</h3>
+      <h3>Name: {name}</h3>
+      <h3>Age: {age}</h3>
+      <h3>S</h3>
+    </div>
+  );
 }
