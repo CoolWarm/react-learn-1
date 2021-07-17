a598acbb6a60c977c79f13daafb9831d2d1a4b71
CoolWarm <78597498+CoolWarm@users.noreply.github.com>
2021-07-17 12:48:08 +0800

    setState (synchronous)
    在组件生命周期或React合成事件中 setState异步
    在setTimeout或原生DOM事件中 setState同步

--- src/App.js
+++ src/App.js
@@ -11,17 +11,31 @@ export default class App extends React.Component {
     return (
       <div>
         <h2>{this.state.message}</h2>
-        <button onClick={e => this.changeText()}>Change Text</button>
+        <button onClick={e => this.changeText()}>Change Text 1</button>
+        <button id="btn">Change Text 2</button>
       </div>
     )
   }
   changeText() {
-    this.setState({
-      message: "AFTER"
-    }, () => { console.log("in setState():", this.state.message) });
-    console.log("after setState():", this.state.message);
+    // setTimeout setState同步
+    setTimeout(() => {
+      this.setState({
+        message: "AFTER"
+      }, () => { console.log("in setState():", this.state.message) });
+      console.log("after setState():", this.state.message);
+    }, 0);
   }
   componentDidUpdate() {
     console.log("componentDidUpdate:", this.state.message);
   }
+  componentDidMount() {
+    // 原生DOM事件中 setState同步
+    document.querySelector("#btn").addEventListener("click", () => {
+      this.setState({
+        message: "AFTER"
+      }, () => { console.log("in setState():", this.state.message) });
+      console.log("after setState():", this.state.message);
+    })
+  }
 }
+// 在 组件生命周期 或 React合成事件 中 setState异步
