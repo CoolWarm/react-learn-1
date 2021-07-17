d9729d9b6afe9ac7001bef29739b72592500b4b7
CoolWarm <78597498+CoolWarm@users.noreply.github.com>
2021-07-17 10:28:37 +0800

    context (class component)
    context 类组件
    跨组件通信

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+const CharacterContext = React.createContext({
+  name: "I don't know",
+  age: 0
+});
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -12,7 +17,9 @@ export default class App extends React.Component {
     return (
       <div>
         <h1>L</h1>
-        <Profile {...this.state} />
+        <CharacterContext.Provider value={this.state}>
+          <Profile />
+        </CharacterContext.Provider>
         <h1>L</h1>
       </div>
     );
@@ -23,20 +30,22 @@ function Profile(props) {
   return (
     <div>
       <h2>M</h2>
-      <Information {...props} />
+      <Information />
       <h2>M</h2>
     </div>
   );
 }
 
-function Information(props) {
-  const {name, age} = props;
-  return (
-    <div>
-      <h3>S</h3>
-      <h3>Name: {name}</h3>
-      <h3>Age: {age}</h3>
-      <h3>S</h3>
-    </div>
-  );
+class Information extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>S</h3>
+        <h3>Name: {this.context.name}</h3>
+        <h3>Age: {this.context.age}</h3>
+        <h3>S</h3>
+      </div>
+    );
+  }
 }
+Information.contextType = CharacterContext;
