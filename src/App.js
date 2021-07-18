f9eb16b096f0654089242705d606f0c0b90cf2f8
CoolWarm <78597498+CoolWarm@users.noreply.github.com>
2021-07-18 11:27:14 +0800

    setState merge
    setState操作会被合并，将多次操作合并成一次

--- src/App.js
+++ src/App.js
@@ -4,38 +4,36 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: "BEFORE"
+      count: 0
     }
   }
   render() {
     return (
       <div>
-        <h2>{this.state.message}</h2>
-        <button onClick={e => this.changeText()}>Change Text 1</button>
-        <button id="btn">Change Text 2</button>
+        <h2>{this.state.count}</h2>
+        <button onClick={e => this.add(1)}> +1 </button>
       </div>
-    )
+    );
   }
-  changeText() {
-    // setTimeout setState同步
-    setTimeout(() => {
-      this.setState({
-        message: "AFTER"
-      }, () => { console.log("in setState():", this.state.message) });
-      console.log("after setState():", this.state.message);
-    }, 0);
-  }
-  componentDidUpdate() {
-    console.log("componentDidUpdate:", this.state.message);
-  }
-  componentDidMount() {
-    // 原生DOM事件中 setState同步
-    document.querySelector("#btn").addEventListener("click", () => {
-      this.setState({
-        message: "AFTER"
-      }, () => { console.log("in setState():", this.state.message) });
-      console.log("after setState():", this.state.message);
-    })
+  add(value) {
+    // setState操作被合并
+    // this.setState({
+    //   count: this.state.count + value
+    // }, ()=>{console.log("+1");});
+    // this.setState({
+    //   count: this.state.count + value
+    // }, ()=>{console.log("+1");});
+
+    // setState合并时进行累加
+    this.setState((prevState, nextProps) => {
+      return {
+        count: prevState.count + 1
+      }
+    }, ()=>{console.log("+1");})
+    this.setState((prevState, nextProps) => {
+      return {
+        count: prevState.count + 1
+      }
+    }, ()=>{console.log("+1");})
   }
 }
-// 在 组件生命周期 或 React合成事件 中 setState异步
