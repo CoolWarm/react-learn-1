26d8d360d6eb44b5b4a07d04eb4fef3ac1f51cc9
CoolWarm <78597498+CoolWarm@users.noreply.github.com>
2021-07-18 16:53:18 +0800

    render() when components are nested
    父组件重新调用render(), 子组件中的render()默认被调用（即使不需要重新调用）

--- src/App.js
+++ src/App.js
@@ -21,13 +21,34 @@ export default class App extends React.Component {
           }
         </ul>
         <button onClick={e => this.addMoive("千と千尋の神隠し")}>Add</button>
+        <FunctionComponent />
+        <ClassComponent />
       </div>
     );
   }
   addMoive(movie) {
     this.setState({
-      // movieList: [...this.state.movieList, movie]
-      movieList: [movie, ...this.state.movieList]
+      movieList: [...this.state.movieList, movie]
     });
+  }
+}
+
+function FunctionComponent(props) {
+  console.log("Function Component");
+  return (
+    <div>
+      <h2>Function Component</h2>
+    </div>
+  );
+}
+
+class ClassComponent extends React.Component {
+  render() {
+    console.log("Class Component");
+    return (
+      <div>
+        <h2>Class Component</h2>
+      </div>
+    );
   }
 }
