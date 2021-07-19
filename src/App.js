643ab629b628afd57701c5748d7d7490b17e95e1
CoolWarm <78597498+CoolWarm@users.noreply.github.com>
2021-07-19 20:34:28 +0800

    Uncontrolled Components in form
    一般情况下推荐使用受控组件，在一个受控组件中，表单数据是由 React 组件来管理的。另一种替代方案是使用非受控组件，这时表单数据将交由 DOM 节点来处理。
    通过ref从DOM节点获取表单数据。

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import React from "react";
 export default class App extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = {
-      username: "",
-      password: "",
-      hobby: "football"
-    }
+
+    this.usernameRef = React.createRef();
   }
   render() {
     return (
@@ -18,31 +15,10 @@ export default class App extends React.PureComponent {
               <input type="text"
                 id="username"
                 name="username"
-                value={this.state.username}
-                onChange={e => this.handleChange(e)}
+                ref={this.usernameRef}
+                // 可设置初始默认值
+                // defaultValue="username"
               />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="password">Password:
-              <input type="password"
-                id="password"
-                name="password"
-                value={this.state.password}
-                onChange={e => this.handleChange(e)}
-              />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="hobby">Hobby
-              <select id="hobby"
-                name="hobby"
-                value={this.state.hobby}
-                onChange={e => this.handleChange(e)}>
-                <option value="baskball">Basketball</option>
-                <option value="football">Football</option>
-                <option value="volleyball">volleyball</option>
-              </select>
             </label>
           </div>
           <input type="submit" value="SUBMIT" />
@@ -50,14 +26,9 @@ export default class App extends React.PureComponent {
       </div>
     );
   }
-  handleChange(event) {
-    this.setState({
-      // 计算属性名 computed property names
-      [event.target.name]: event.target.value
-    });
-  }
   handleSubimt(event) {
     event.preventDefault();
-    console.log(this.state.username, this.state.password, this.state.hobby);
+    const username = this.usernameRef.current.value;
+    console.log(username);
   }
 }
