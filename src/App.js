f4d3f88b262a5f3452b37cb1355be0abd51d73ee
CoolWarm <78597498+CoolWarm@users.noreply.github.com>
2021-07-20 15:43:03 +0800

    higher order component -- authentication
    在高阶组件中判断，引导至合适的组件并渲染

--- src/App.js
+++ src/App.js
@@ -1,63 +1,40 @@
-import React, { createContext } from "react";
-
-const UserInformationContext = createContext({
-  level: 0,
-  region: "Earth"
-});
+import React from "react";
 
 export default class App extends React.PureComponent {
   render() {
     return (
       <div>
         <h1>APP</h1>
-        <UserInformationContext.Provider value={{level: 99, region:"Britain"}}>
-          <UserProfile name="Harry Potter" age={18} />
-          <UserProfile name="Sherlock Holmes" age={26} />
-          <UserIntro rank="Boss" />
-        </UserInformationContext.Provider>
+        <AuthMePage isLogin={true} />
       </div>
     );
   }
 }
 
-function withUserInformation(WrappedComponent) {
+function withAuth(WrappedComponent) {
   return (props) => {
-    return (
-      <UserInformationContext.Consumer>
-        {
-          value => <WrappedComponent {...props} {...value}/>
-        }
-      </UserInformationContext.Consumer>
-    );
+    if (props.isLogin) {
+      return <WrappedComponent {...props} />
+    } else {
+      return <LoginPage />
+    }
   }
 }
 
-class Profile extends React.PureComponent {
+class MePage extends React.PureComponent {
   render() {
     return (
-      <div>
-        <h2>Name: {this.props.name}</h2>
-        <h2>Age: {this.props.age}</h2>
-        <h2>Level: {this.props.level}</h2>
-        <h2>Region: {this.props.region}</h2>
-        <hr />
-      </div>
-    );
+      <h2>ME</h2>
+    )
   }
 }
 
-const UserProfile = withUserInformation(Profile);
+const AuthMePage = withAuth(MePage);
 
-function Intro(props) {
-  return (
-    <div>
-      <ul>
-        <li>Rank: {props.rank}</li>
-        <li>Level: {props.level}</li>
-        <li>Region: {props.region}</li>
-      </ul>
-    </div>
-  );
+class LoginPage extends React.PureComponent {
+  render() {
+    return (
+      <h2>LOG IN</h2>
+    )
+  }
 }
-
-const UserIntro = withUserInformation(Intro);
