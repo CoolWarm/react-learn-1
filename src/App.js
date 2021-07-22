8b07e4a52afa8eee8d98fcbdadd3a4ad279f0261
CoolWarm <78597498+CoolWarm@users.noreply.github.com>
2021-07-22 10:43:07 +0800

    AntDesign
    antd

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from "react";
-import classNames from "classnames";
+
+import moment from 'moment';
+import { Button, Menu, Dropdown, DatePicker } from "antd";
 
 export default class App extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      isActive: true,
+
     }
   }
   render() {
-    const { isActive } = this.state;
+    const menu = (
+      <Menu onClick={e => this.handleMenuClick(e)}>
+        <Menu.Item key="1">1st item</Menu.Item>
+        <Menu.Item key="2">2nd item</Menu.Item>
+        <Menu.Item key="3">3rd item</Menu.Item>
+      </Menu>
+    );
     return (
       <>
-        <h1 className={classNames("title", { active: isActive }, ["important", "primary"], true, null, undefined, 0, 99)}>Hello World</h1>
+        <Button type="primary">primary</Button>
+        <Button>secondary</Button>
+        <Dropdown.Button overlay={menu}>Actions</Dropdown.Button>
+        <DatePicker allowClear={false} />
+        <DatePicker defaultValue={moment('2030/01/01', "YYYY/MM/DD")} format="YYYY/MM/DD" />
       </>
     );
   }
+  handleMenuClick(e) {
+    console.log('click', e);
+  }
 }
+
